94d5d8e4b24c560eed18b7b273eb05c0b7808f24
funnoke <freecell.phi-brain@mail.ru>
2021-06-05 22:48:11 +0300

    Update general_tree and test for general_tree

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,9 @@ void test_general_tree();
 void test_bst();
 
 int main() {
-	cout << "==== BST Test ====" << endl;
+	std::cout << "==== BST Test ====" << std::endl;
 	test_bst();
-	cout << endl << "==== General Tree Test ====" << endl;
+	std::cout << std::endl << "==== General Tree Test ====" << std::endl;
 	test_general_tree();
 }
 
@@ -97,48 +97,71 @@ void test_bst() {
 }
 
 void test_general_tree() {
+
 	tree_general<int> test_int;
 	tree_general<int> test_int_2;
-	tree_g<int> *test_find_int;
+	tree_g<int>* test_find_int;
+
+	link_list<float>* list_float;
 	tree_general<float> test_float;
 	tree_general<float> test_float_2;
-	tree_g<float> *test_find_float;
+	tree_g<float>* test_find_float;
+
+	link_list<char>* list_char;
 	tree_general<char> test_char;
 	tree_general<char> test_char_2;
-	tree_g<char> *test_find_char;
-	//Тест для test_int
-	for (int i = 0; i < 43; ++i) {
+	tree_g<char>* test_find_char;
+
+	//Test for test_int
+	std::cout << "Test int:" << std::endl;
+	for (int i = 0; i < 100; ++i) {
 		if (i < 5) {
 			test_int.push((i - 1), i);
 			test_int_2.push((i - 1), i);
-		} else if (i < 10) {
+		}
+		else if (i < 10) {
 			test_int.push(1, i);
 			test_int_2.push(1, i);
-		} else if (i < 20) {
+		}
+		else if (i < 20) {
 			test_int.push(i % 10, i);
 			test_int_2.push(i % 10, i);
-		} else if (i == 20) {
+		}
+		else if (i == 20) {
 			std::cout << "The firsh tree after 'push' is " << std::endl;
 			test_int.print();
 			std::cout << "Height of tree is " << test_int.height() << std::endl;
-		} else if (i < 30) {
+		}
+		else if (i < 30) {
 			test_int.pop(i % 10);
-		} else if (i == 30) {
+		}
+		else if (i == 30) {
 			std::cout << "The firsh tree after 'pop' is " << std::endl;
 			test_int.print();
-		} else if (i < 35) {
+		}
+		else if (i < 35) {
 			std::cout << "Amount of tree is " << test_int.count() << std::endl;
 			test_int.pop((i % 10) + 10);
-		} else if (i == 35) {
+		}
+		else if (i == 35) {
+			test_int.push(16, 16);
 			test_int.print();
-		} else if (i < 41) {
-			std::cout << "Path from peek to " << (i % 10 + 10) << " is " << test_int.path((i % 10) + 10) << std::endl;
-
-		} else if (i == 41) {
+		}
+		else if (i < 41) {
+			Vector<int> V;
+			V = test_int.path(&V, (i % 10 + 10));
+			std::cout << "Path from peek to " << (i % 10 + 10) << " is " << V << std::endl;
+		}
+		else if (i == 41) {
+			test_int.pop(16);
 			test_find_int = test_int.find(i % 10 - 1);
-			test_find_int = test_find_int->son;
+			if (test_find_int->son == nullptr)
+				test_find_int == nullptr;
+			else
+				test_find_int = test_find_int->son;
 			std::cout << "Find element " << (i % 10 - 1) << ". It son is " << test_find_int->data << "." << std::endl;
-		} else if (i == 42) {
+		}
+		else if (i == 42) {
 			std::cout << "First tree is" << std::endl;
 			test_int.print();
 
@@ -169,6 +192,305 @@ void test_general_tree() {
 			else
 				std::cout << "false" << std::endl;
 		}
+		else if (i == 43) {
+			link_list<int> list_int;
+			test_int.conv_tree_to_linklist(&list_int);
+			std::cout << "Converted tree to linked list. List is ";
+			list_int.print(&list_int);
+			std::cout << std::endl;
+			delete_list(&list_int);
+		}
+		else if (i < 54) {
+			test_int_2.pop(i % 10 + 10);
+			if (i > 43 && i < 48)
+				test_int.push(0, i % 4 + 11);
+		}
+		else if (i == 54) {
+			tree_general<int> test_int_empty;
+
+			std::cout << "First tree is" << std::endl;
+			test_int.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_int_2.print();
+
+			std::cout << "Addition second tree with first tree." << std::endl;
+			test_int_2.addition_tree(&test_int);
+
+			std::cout << "First tree is" << std::endl;
+			test_int.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_int_2.print();
+
+			std::cout << "Addition first tree with empty tree" << std::endl;
+			test_int.addition_tree(&test_int_empty);
+
+			std::cout << "First tree is" << std::endl;
+			test_int.print();
+
+			std::cout << "Addition first tree with first tree" << std::endl;
+			test_int.addition_tree(&test_int);
+
+			std::cout << "First tree is" << std::endl;
+			test_int.print();
+		}
 
 	}
+
+	std::cout << "\n\n\n\n\nTest float:" << std::endl;
+	//Test for test_float
+	for (int i = 0; i < 100; ++i) {
+		if (i < 5) {
+			test_float.push((i - 1), i);
+			test_float_2.push((i - 1), i);
+		}
+		else if (i < 10) {
+			test_float.push(1, i);
+			test_float_2.push(1, i);
+		}
+		else if (i < 20) {
+			test_float.push(i % 10, i);
+			test_float_2.push(i % 10, i);
+		}
+		else if (i == 20) {
+			std::cout << "The firsh tree after 'push' is " << std::endl;
+			test_float.print();
+			std::cout << "Height of tree is " << test_float.height() << std::endl;
+		}
+		else if (i < 30) {
+			test_float.pop(i % 10);
+		}
+		else if (i == 30) {
+			std::cout << "The firsh tree after 'pop' is " << std::endl;
+			test_float.print();
+		}
+		else if (i < 35) {
+			std::cout << "Amount of tree is " << test_float.count() << std::endl;
+			test_float.pop((i % 10) + 10);
+		}
+		else if (i == 35) {
+			test_float.push(16, 16);
+			test_float.print();
+		}
+		else if (i < 41) {
+			Vector<int> V;
+			V = test_float.path(&V, (i % 10 + 10));
+			std::cout << "Path from peek to " << (i % 10 + 10) << " is " << V << std::endl;
+		}
+		else if (i == 41) {
+			test_float.pop(16);
+			test_find_float = test_float.find(i % 10 - 1);
+			if (test_find_float->son == nullptr)
+				test_find_float == nullptr;
+			else
+				test_find_float = test_find_float->son;
+			std::cout << "Find element " << (i % 10 - 1) << ". It son is " << test_find_float->data << "." << std::endl;
+		}
+		else if (i == 42) {
+			std::cout << "First tree is" << std::endl;
+			test_float.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_float_2.print();
+
+			std::cout << "First < Second is ";
+			if (test_float < test_float_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+
+			std::cout << "First > Second is ";
+			if (test_float > test_float_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+
+			std::cout << "First == Second is ";
+			if (test_float == test_float_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+
+			std::cout << "First <= Second is ";
+			if (test_float <= test_float_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+		}
+		else if (i == 43) {
+			link_list<float> list_float;
+			test_float.conv_tree_to_linklist(&list_float);
+			std::cout << "Converted tree to linked list. List is ";
+			list_float.print(&list_float);
+			std::cout << std::endl;
+			delete_list(&list_float);
+		}
+		else if (i < 54) {
+			test_float_2.pop(i % 10 + 10);
+			if (i > 43 && i < 48)
+				test_float.push(0, i % 4 + 11);
+		}
+		else if (i == 54) {
+			tree_general<float> test_float_empty;
+
+			std::cout << "First tree is" << std::endl;
+			test_float.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_float_2.print();
+
+			std::cout << "Addition second tree with first tree." << std::endl;
+			test_float_2.addition_tree(&test_float);
+
+			std::cout << "First tree is" << std::endl;
+			test_float.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_float_2.print();
+
+			std::cout << "Addition first tree with empty tree" << std::endl;
+			test_float.addition_tree(&test_float_empty);
+
+			std::cout << "First tree is" << std::endl;
+			test_float.print();
+
+			std::cout << "Addition first tree with first tree" << std::endl;
+			test_float.addition_tree(&test_float);
+
+			std::cout << "First tree is" << std::endl;
+			test_float.print();
+		}
+
+	}
+
+	std::cout << "\n\n\n\n\nTest char:" << std::endl;
+	//Test for test_char
+	for (int j = 60, i = 60; i < 200; ++i) {
+		if (i < (j + 5)) {
+			test_char.push((i - 1), i);
+			test_char_2.push((i - 1), i);
+		}
+		else if (i < (j + 10)) {
+			test_char.push(i - 5, i);
+			test_char_2.push(i - 5, i);
+		}
+		else if (i < (j + 20)) {
+			test_char.push(i % 10 + j, i);
+			test_char_2.push(i % 10 + j, i);
+		}
+		else if (i == (j + 20)) {
+			std::cout << "The firsh tree after 'push' is " << std::endl;
+			test_char.print();
+			std::cout << "Height of tree is " << test_char.height() << std::endl;
+		}
+		else if (i < (j + 30)) {
+			test_char.pop(i % 10 + j);
+		}
+		else if (i == (j + 30)) {
+			std::cout << "The firsh tree after 'pop' is " << std::endl;
+			test_char.print();
+		}
+		else if (i < (j + 35)) {
+			std::cout << "Amount of tree is " << test_char.count() << std::endl;
+			test_char.pop((i % 10) + j + 10);
+		}
+		else if (i == (j + 35)) {
+			test_char.push(j, j);
+			test_char.print();
+		}
+		else if (i < (j + 41)) {
+			Vector<int> V;
+			V = test_char.path(&V, j);
+			std::cout << "Path from peek to " << j << " is " << V << std::endl;
+		}
+		else if (i == (j + 41)) {
+			test_char.pop(j);
+			test_find_char = test_char.find(i % 10 - 1 + j);
+			if (test_find_char->son == nullptr) {
+				test_find_char == nullptr;
+				std::cout << "Find element " << (i % 10 - 1 + j) << ". It son no detected." << std::endl;
+			}
+			else {
+				test_find_char = test_find_char->son;
+				std::cout << "Find element " << (i % 10 - 1 + j) << ". It son is " << test_find_char->data << "." << std::endl;
+			}
+		}
+		else if (i == (j + 42)) {
+			std::cout << "First tree is" << std::endl;
+			test_char.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_char_2.print();
+
+			std::cout << "First < Second is ";
+			if (test_char < test_char_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+
+			std::cout << "First > Second is ";
+			if (test_char > test_char_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+
+			std::cout << "First == Second is ";
+			if (test_char == test_char_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+
+			std::cout << "First <= Second is ";
+			if (test_char <= test_char_2)
+				std::cout << "true" << std::endl;
+			else
+				std::cout << "false" << std::endl;
+		}
+		else if (i == (j + 43)) {
+			link_list<char> list_char;
+			test_char.conv_tree_to_linklist(&list_char);
+			std::cout << "Converted tree to linked list. List is ";
+			list_char.print(&list_char);
+			std::cout << std::endl;
+			delete_list(&list_char);
+		}
+		else if (i < (j + 54)) {
+			test_char_2.pop(i % 10 + 10 + j);
+			if (i > (j + 43) && i < (j + 48))
+				test_char.push(j, i % 4 + 11 + j);
+		}
+		else if (i == (j + 54)) {
+			tree_general<char> test_char_empty;
+
+			std::cout << "First tree is" << std::endl;
+			test_char.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_char_2.print();
+
+			std::cout << "Addition second tree with first tree." << std::endl;
+			test_char_2.addition_tree(&test_char);
+
+			std::cout << "First tree is" << std::endl;
+			test_char.print();
+
+			std::cout << "Secont tree is" << std::endl;
+			test_char_2.print();
+
+			std::cout << "Addition first tree with empty tree" << std::endl;
+			test_char.addition_tree(&test_char_empty);
+
+			std::cout << "First tree is" << std::endl;
+			test_char.print();
+
+			std::cout << "Addition first tree with first tree" << std::endl;
+			test_char.addition_tree(&test_char);
+
+			std::cout << "First tree is" << std::endl;
+			test_char.print();
+		}
+
+	}
+
 }
--- src/tree_general.h
+++ src/tree_general.h
@@ -1,38 +1,65 @@
 #ifndef TREE__GENERAL
 #define TREE__GENERAL
 #include <iostream>
+#include "Vector.h"
+
 template <typename T>
 struct tree_g {
-	T data;
-	tree_g<T> *son;
-	tree_g<T> *brother;
-	tree_g() {
-		this->data = 0;
-		this->son = nullptr;
-		this->brother = nullptr;
-
-	}
-	tree_g(T data) {
-		this->data = data;
-		this->son = nullptr;
-		this->brother = nullptr;
-	}
+	T data = 0;
+	tree_g<T>* son = nullptr;
+	tree_g<T>* brother = nullptr;
+	tree_g() {};
+	tree_g(T data) : data(data) {};
 };
+
+template <typename T>
+struct link_list {
+public:
+	T data;
+	link_list<T>* next_data;
+	link_list() {
+		this->data = 0;
+		this->next_data = nullptr;
+	}
+	link_list(T data) {
+		this->data = data;
+		this->next_data = nullptr;
+	}
+	void print(link_list<T>* list) {
+		if (list == nullptr)
+			return;
+		std::cout << "   " << list->data;
+		if (list->next_data != 0)
+			print(list->next_data);
+		return;
+	}
+
+};
+template <typename T>
+void delete_list(link_list<T>* L) {
+	if (L == nullptr)
+		return;
+	if (L->next_data != nullptr) {
+		delete_list(L->next_data);
+		delete L->next_data;
+		return;
+	}
+}
 
 template <typename T>
 class tree_general {
 private://Поля
-	tree_g<T> *unit;
+	tree_g<T>* unit = nullptr;
 
 private://Методы
-	void delete_tree(tree_g<T> *unit) {
+	void delete_tree(tree_g<T>* unit) {
 		if (unit == nullptr)
 			return;
 		delete_tree(unit->son);
 		delete_tree(unit->brother);
 		delete unit;
 	}
-	void push(tree_g<T> *unit, T node, T data) {
+	void push(tree_g<T>* unit, T node, T data) {
 		//Поиск узла
 		if (unit == nullptr) {
 			this->unit = new tree_g<T>(data);
@@ -53,11 +80,11 @@ private://Методы
 		return;
 		std::cout << unit->data << std::endl;
 	}
-	T pop(tree_g<T> *unit, T element) {
+	T pop(tree_g<T>* unit, T element) {
 		T data_element = 0;
-		tree_g<T> *local_element;
-		tree_g<T> *before_local;
-		tree_g<T> *local;
+		tree_g<T>* local_element;
+		tree_g<T>* before_local;
+		tree_g<T>* local;
 		//существует ли элемент
 		local_element = find_element(unit, element);
 		before_local = find_unit_before_son(unit, local_element);
@@ -72,25 +99,28 @@ private://Методы
 					delete local_element;
 					return data_element;
 				}
-			} else {
+			}
+			else {
 				//Есть ли сыновья у удаляемого элемента?
 				if (local_element->son != 0) {//да
 					//Есть ли сын у предыдущего элемента?
 					if (before_local->son != 0) {//да
 						local = before_local->son;
 						//Находим последнего брата этого сына
-						while (local->brother == 0) {
+						while (local->brother != 0) {
 							local = local->brother;
 						}
 						local->brother = local_element->son;
-					} else {//нет
+					}
+					else {//нет
 						before_local->son = local_element->son;
 					}
 				}
 				//Есть ли брат у удоляемого элемента
 				if (local_element->brother != 0) {
 					before_local->brother = local_element->brother;
-				} else {
+				}
+				else {
 					before_local->brother = nullptr;
 				}
 				local_element->son = nullptr;
@@ -98,7 +128,8 @@ private://Методы
 				data_element = local_element->data;
 				delete local_element;
 			}
-		} else {
+		}
+		else {
 			//Есть ли брат у удаляемого элемента
 			if (local_element->brother != 0) {//да
 				//Есть ли сын у удаляемого элемента
@@ -110,16 +141,19 @@ private://Методы
 						while (local->brother != 0)
 							local = local->brother;
 						local->brother = local_element->son;
-					} else {//нет
+					}
+					else {//нет
 						local->son = local_element->son;
 					}
 				}
 				before_local->son = local_element->brother;
-			} else {//нет
-				//Есть ли сын у удаляемого элемента
+			}
+			else {//нет
+			 //Есть ли сын у удаляемого элемента
 				if (local_element->son != 0) {
 					before_local->son = local_element->son;
-				} else {
+				}
+				else {
 					before_local->son = nullptr;
 				}
 			}
@@ -132,9 +166,8 @@ private://Методы
 		return data_element;
 
 	}
-
-	tree_g<T> *find_unit_before_brother(tree_g<T> *unit, tree_g<T> *element) {
-		tree_g<T> *local = nullptr;
+	tree_g<T>* find_unit_before_brother(tree_g<T>* unit, tree_g<T>* element) {
+		tree_g<T>* local = nullptr;
 		if (unit == nullptr)
 			return 0;
 		if (unit->brother == element)
@@ -148,8 +181,8 @@ private://Методы
 		return local;
 
 	}
-	tree_g<T> *find_unit_before_son(tree_g<T> *unit, tree_g<T> *element) {
-		tree_g<T> *local = nullptr;
+	tree_g<T>* find_unit_before_son(tree_g<T>* unit, tree_g<T>* element) {
+		tree_g<T>* local = nullptr;
 		if (unit == nullptr)
 			return 0;
 		if (unit->son == element)
@@ -163,8 +196,8 @@ private://Методы
 		return local;
 
 	}
-	tree_g<T> *find_element(tree_g<T> *unit, T element) {
-		tree_g<T> *local = nullptr;
+	tree_g<T>* find_element(tree_g<T>* unit, T element) {
+		tree_g<T>* local = nullptr;
 		if (unit == nullptr)
 			return 0;
 		if (unit->data == element)
@@ -181,7 +214,7 @@ private://Методы
 			throw std::underflow_error("Element is not found");
 		return local;
 	}
-	int count(tree_g<T> *unit) {
+	int count(tree_g<T>* unit) {
 		int sum = 0;
 		if (unit == nullptr)
 			return 0;
@@ -190,38 +223,76 @@ private://Методы
 		sum++;
 		return sum;
 	}
-	int height(tree_g<T> *unit) {
+	int height(tree_g<T>* unit) {
 		if (unit == 0)
 			return 0;
 		int height_max = 0;
-		for (tree_g<T> *unit_local = unit->son; unit_local; unit_local = unit_local->brother) {
+		for (tree_g<T>* unit_local = unit->son; unit_local; unit_local = unit_local->brother) {
 			int height_local = height(unit_local);
 			if (height_local > height_max)
 				height_max = height_local;
 		}
 		return ++height_max;
 	}
-	T path(tree_g<T> *unit, T node) {
+
+	Vector<int>* path(Vector<int>* V, tree_g<T>* unit, T node) {
+		if (unit->data == node) {
+			V->push_back(path_element(this->unit, unit) - 1);
+		}
+		if (unit->son != 0)
+			path(V, unit->son, node);
+		if (unit->brother != 0)
+			path(V, unit->brother, node);
+		return V;
+	}
+	int path_element(tree_g<T>* unit, tree_g<T>* node) {
 		if (unit == nullptr)
 			return 0;
 		int local = 0;
-		if (unit->data == node)
+		if (unit == node)
 			return 1;
-		local = path(unit->son, node);
-		if (local != 0) {
+		if (local == 0)
+			local = path_element(unit->son, node);
+		if (local != 0)
+		{
 			++local;
 			return local;
 		}
-		local = path(unit->brother, node);
-		if (local != 0) {
+		if (local == 0)
+			local = path_element(unit->brother, node);
+		if (local != 0)
+		{
 			return local;
 		}
 		if (local == 0 && unit == this->unit)
 			throw std::underflow_error("Element is not found");
 		return local;
-
 	}
-	void print(tree_g<T> *unit, int count_scape) {
+
+	int path_element(tree_g<T>* unit, T node) {
+		if (unit == nullptr)
+			return 0;
+		int local = 0;
+		if (unit->data == node)
+			return 1;
+		if (local == 0)
+			local = path_element(unit->son, node);
+		if (local != 0)
+		{
+			++local;
+			return local;
+		}
+		if (local == 0)
+			local = path_element(unit->brother, node);
+		if (local != 0)
+		{
+			return local;
+		}
+		if (local == 0 && unit == this->unit)
+			throw std::underflow_error("Element is not found");
+		return local;
+	}
+	void print(tree_g<T>* unit, int count_scape) {
 		if (unit == 0)
 			return;
 		for (int i = 0; i < count_scape; i++)
@@ -237,10 +308,86 @@ private://Методы
 		}
 
 	}
-public:
-	tree_general() {
-		unit = nullptr;
+
+	//Individual tasks
+	link_list<T>* conv_tree_to_linklist(link_list<T>* unit_list, tree_g<T>* unit_tree) {
+		if (unit_tree == nullptr)
+			throw std::underflow_error("Tree is empty");
+		unit_list->data = unit_tree->data;
+		if (unit_tree->son != 0) {
+			unit_list->next_data = new link_list<T>;
+			unit_list = conv_tree_to_linklist(unit_list->next_data, unit_tree->son);
+		}
+		if (unit_tree->brother != 0) {
+			unit_list->next_data = new link_list<T>;
+			unit_list = conv_tree_to_linklist(unit_list->next_data, unit_tree->brother);
+		}
+		return unit_list;
 	}
+	void addition_tree(tree_g<T>* unit, tree_g<T>* sec_unit) {
+		if (unit == nullptr)
+			if (sec_unit == nullptr) {
+				//unit - 0, sec_unit - 0
+				return;
+			}
+			else {//unit - 0, sec_unit - no empty
+				this->unit = new tree_g<T>;
+				unit = this->unit;
+				addition_tree_push(unit, sec_unit);
+				return;
+			}
+		else
+			if (sec_unit == nullptr) {
+				//unit - no empty, sec_unit - 0
+				return;
+			}
+			else {//unit - no empty, sec_unit - no empty
+				if (unit == sec_unit) {
+					tree_general<T> local;
+					local.addition_tree(local.unit, sec_unit);
+					if (unit->son != 0) {
+						unit = unit->son;
+						while (unit->brother != 0) {
+							unit = unit->brother;
+						}
+						unit->brother = new tree_g<T>();
+						addition_tree_push(unit->brother, local.unit);
+					}
+					else {
+						unit->son = new tree_g<T>();
+						addition_tree_push(unit->son, local.unit);
+					}
+					return;
+				}
+				else {
+					if (unit->son != 0) {
+						unit = unit->son;
+						while (unit->brother != 0) {
+							unit = unit->brother;
+						}
+						unit->brother = new tree_g<T>();
+						addition_tree_push(unit->brother, sec_unit);
+					}
+					else {
+						unit->son = new tree_g<T>();
+						addition_tree_push(unit->son, sec_unit);
+					}
+					return;
+				}
+			}
+	}
+	void addition_tree_push(tree_g<T>* unit, tree_g<T>* sec_unit) {
+		unit->data = sec_unit->data;
+		if (sec_unit->son != 0) {
+			unit->son = new tree_g<T>();
+			addition_tree_push(unit->son, sec_unit->son);
+		}
+		if (sec_unit->brother != 0) {
+			unit->brother = new tree_g<T>();
+			addition_tree_push(unit->brother, sec_unit->brother);
+		}
+	}
+public:
 	~tree_general() {
 		delete_tree(unit);
 	}
@@ -253,7 +400,7 @@ public:
 	int count() {
 		return count(unit);
 	}
-	tree_g<T> *find(T element) {
+	tree_g<T>* find(T element) {
 		return find_element(unit, element);
 	}
 	int height() {
@@ -262,32 +409,39 @@ public:
 	void print() {
 		print(unit, 0);
 	}
-	T path(T node) {
-		return path(unit, node);
+	Vector<int>* path(Vector<int>* V, T node) {
+		return path(V, unit, node);
 	}
 
+	//Individual tasks
+	void conv_tree_to_linklist(link_list<T>* unit_list) {
+		conv_tree_to_linklist(unit_list, unit);
+	}
+	void addition_tree(tree_general<T>* sec_tree) {
+		addition_tree(unit, sec_tree->unit);
+	}
 
-	bool operator<(tree_general<T> &second_tree) {
+	bool operator<(tree_general<T>& second_tree) {
 		if (this == &second_tree)
 			return false;
 
 		return (count() < second_tree.count());
 	}
-	bool operator>(tree_general<T> &second_tree) {
+	bool operator>(tree_general<T>& second_tree) {
 		return (second_tree < *this);
 	}
-	bool operator==(tree_general<T> &second_tree) {
+	bool operator==(tree_general<T>& second_tree) {
 		if (this == &second_tree)
 			return true;
 		return (count() == second_tree.count());
 	}
-	bool operator!=(tree_general<T> &second_tree) {
+	bool operator!=(tree_general<T>& second_tree) {
 		return !(*this == second_tree);
 	}
-	bool operator<=(tree_general<T> &second_tree) {
+	bool operator<=(tree_general<T>& second_tree) {
 		return ((*this < second_tree) || (*this == second_tree));
 	}
-	bool operator>=(tree_general<T> &second_tree) {
+	bool operator>=(tree_general<T>& second_tree) {
 		return ((*this > second_tree) || (*this == second_tree));
 	}
 
